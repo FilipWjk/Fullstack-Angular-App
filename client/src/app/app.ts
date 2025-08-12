14b64341754a1db8051a9b473813a6d3621fb52d
Filip <wojtasik.f03@htlwienwest.at>
2025-08-12 19:37:37 +0200

    (conf): establish first communication from server to client

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('fullstack-angular-app');
+  private apiService = inject(ApiService);
+  protected readonly title = signal('Fullstack Angular & Nest.js App');
+  protected message = signal<string>('');
+
+  constructor() {
+    this.apiService.getHello().subscribe({
+      next: (msg) => {
+        return this.message.set(msg);
+      },
+      error: (err) => this.message.set('Error: ' + (err?.message || 'Unknown')),
+    });
+  }
 }
