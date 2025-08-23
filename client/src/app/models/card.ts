93c1b241aa99d7bf4acf2639b014458bc8e779ab
Filip <wojtasik.f03@htlwienwest.at>
2025-08-23 21:57:22 +0200

    (feat): add Card interface to define card model structure

---
+++ client/src/app/models/card.ts
@@ -0,0 +1,10 @@
+export interface Card {
+  id: number;
+  title: string;
+  image: string;
+  rating: number;
+  description: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+}
